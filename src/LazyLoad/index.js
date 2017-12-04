019b44b0613949421f8c87ff71eaac7ce97770e6
zaynex <zaynex@126.com>
2017-12-04 20:08:43 +0800

    FIX: find parentNode

--- src/LazyLoad/index.js
+++ src/LazyLoad/index.js
@@ -1,7 +1,8 @@
-import React, { PureComponent,Component } from 'react'
+import React, { PureComponent, Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import { debounce } from 'lodash'
+import scrollParent from './scrollParent'
 import './index.css'
 
 
@@ -42,10 +43,12 @@ export default class LazyLoad extends PureComponent {
   }
 }
 
-let finalLazyLoadHandle = null
+let finalLazyLoadHandler = null
 class Lazy extends Component {
   static propTypes = {
     offset: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
+    children: PropTypes.object,
+    placeholder: PropTypes.object,
   }
   static defaultProps = {
     offset: 0
@@ -55,17 +58,17 @@ class Lazy extends Component {
     this.isVisible = false
   }
 
-  shouldComponentUpdate() {
+  shouldComponentUpdate () {
     return this.isVisible
   }
   componentDidMount () {
     const node = ReactDOM.findDOMNode(this)
-    const parentNode = node.parentNode
+    const parentNode = scrollParent(node)
 
-    finalLazyLoadHandle = debounce(lazyLoadHandlers, 300)
+    finalLazyLoadHandler = debounce(lazyLoadHandlers, 300)
     const listenerCount = 1 + (+parentNode.getAttribute(LISTEN_FLAG))
-    if(listenerCount == 1) {
-      parentNode.addEventListener('scroll', finalLazyLoadHandle, false)
+    if (listenerCount == 1) {
+      parentNode.addEventListener('scroll', finalLazyLoadHandler, false)
     }
     parentNode.setAttribute(LISTEN_FLAG, listenerCount)
 
@@ -73,8 +76,25 @@ class Lazy extends Component {
     checkInvision(this)
   }
 
+  componentWillUnmount () {
+    const parent = scrollParent(ReactDOM.findDOMNode(this))
+    if (parent && typeof parent.getAttribute === 'function') {
+      const listenerCount = (+parent.getAttribute(LISTEN_FLAG)) - 1
+      if (listenerCount === 0) {
+        parent.removeEventListener('scroll', finalLazyLoadHandler, false)
+        parent.removeAttribute(LISTEN_FLAG)
+      } else {
+        parent.setAttribute(LISTEN_FLAG, listenerCount)
+      }
+    }
+    const index = listeners.indexOf(this);
+    if (index !== -1) {
+      listeners.splice(index, 1);
+    }
+  }
+
   render () {
-    return !this.isVisible ? <div style={{width: 100, height: 100}}>I am loading</div> : this.props.children
+    return !this.isVisible ? (this.props.placeholder ? this.props.placeholder : <PlaceHolder />) : this.props.children
   }
 }
 class Item extends PureComponent {
@@ -86,36 +106,35 @@ class Item extends PureComponent {
 }
 
 
-const defaultBoundingClintRect = { top: 0, right: 0, bottom: 0, left: 0, width: 0, height: 0 }
-
+const PlaceHolder = () => <div style={{ width: 100, height: 100 }}>I am loading</div>
 const lazyLoadHandlers = () => {
-  for(let i = 0; i < listeners.length; ++i) {
+  for (let i = 0; i < listeners.length; ++i) {
     const listener = listeners[i]
     checkInvision(listener)
   }
+  purgePending()
 }
 
-const purgePending = function purgePending() {
+const purgePending = function purgePending () {
   pending.forEach((component) => {
     const index = listeners.indexOf(component)
-    if(index !== -1) listeners.splice(index, 1)
+    if (index !== -1) listeners.splice(index, 1)
   })
   pending = []
 }
 
 const checkInvision = (component) => {
   const node = ReactDOM.findDOMNode(component)
-  const parentNode = node.parentNode
-  if(component.isVisible) return
+  const parentNode = scrollParent(node)
+  if (component.isVisible) return
   const visible = checkOverflowVisbile(component, parentNode)
-  if(visible) {
-    if(!component.isVisible) {
-      if(component.props.once) {
-        pending.push(component)
-      }
+  if (visible) {
+    if (!component.isVisible) {
       component.isVisible = true
       component.forceUpdate()
     }
+  } else {
+    component.isVisible = false
   }
 }
 
@@ -127,13 +146,13 @@ const checkOverflowVisbile = (component, parentNode) => {
   try {
     ({ top: parentTop, height: parentHeight } = parentNode.getBoundingClientRect())
   } catch (e) {
-    ({ top: parentTop, height: parentHeight } = defaultBoundingClintRect)
+    ({ top: parentTop, height: parentHeight } = defaultBoundingClientRect)
   }
 
   const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight
 
   const interSectionTop = Math.max(parentTop, 0)
-  const interSectionHeight = Math.min(window.innerHeight, parentTop + parentHeight) - interSectionTop
+  const interSectionHeight = Math.min(windowInnerHeight, parentTop + parentHeight) - interSectionTop
 
   console.log(`[interSectionTop]`, interSectionTop)
   console.log(`[interSectionHeight]`, interSectionHeight)
@@ -143,7 +162,7 @@ const checkOverflowVisbile = (component, parentNode) => {
   try {
     ({ top, height } = node.getBoundingClientRect())
   } catch (e) {
-    ({ top, height } = defaultBoundingClintRect)
+    ({ top, height } = defaultBoundingClientRect)
   }
 
   const offsetTop = top - interSectionTop
@@ -153,6 +172,3 @@ const checkOverflowVisbile = (component, parentNode) => {
 
   return (offsetTop - offsets[0] <= interSectionHeight) && (offsetTop + height + offsets[1] >= 0)
 }
-
-
-window.listeners = listeners
